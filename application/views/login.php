2c3ecb01e4af2ecdb5ec68a8c212c9bbcecc7446
Spencer Pollock <srepollock@gmail.com>
2016-03-24 16:19:41 -0700

    :smile: Login working. Dont know with no visuals. Now to add authorization.

--- application/views/login.php
+++ application/views/login.php
@@ -1,5 +1,5 @@
 <form name="login" method="post" action="/auth/submit">
     UserID: <input type="text" name="userid" placeholder="User"></input><br />
     Password: <input type="text" name="password" placeholder="Password"></input><br />
-    <input type="submit">Submit</input>
+    <input type="submit" />
 </form>
