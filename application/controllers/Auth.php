f84c50968b2ffc3abc015f6ce8b9cbbadf6aa3a2
dseo517 <danny@bcwireless.ca>
2016-04-01 19:50:47 -0700

    added authentication

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -13,10 +13,18 @@
 			$key = $_POST['userid'];
 			$user = $this->users->get($key);
 			if (password_verify($this->input->post('password'),$user->password)) {
-			$this->session->set_userdata('userID',$key);
-			$this->session->set_userdata('userName',$user->name);
-			$this->session->set_userdata('userRole',$user->role);
+				$this->session->set_userdata('userID',$key);
+				$this->session->set_userdata('userName',$user->name);
+				$this->session->set_userdata('userRole',$user->role);
+				redirect('/');
 			}
+			else{
+				redirect("$key");
+			}
+			
+		}
+		function logout() {
+			$this->session->sess_destroy();
 			redirect('/');
 		}
 	}
